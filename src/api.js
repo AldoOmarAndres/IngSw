385898c14800fa939170ba5e627daeabead1b5a1
Aldo <aldoandres045@gmail.com>
2024-05-18 20:47:22 -0300

    Deploy a Azure

--- src/api.js
+++ src/api.js
@@ -2,5 +2,10 @@ export async function traerChiste() {
   const chiste = await fetch("https://v2.jokeapi.dev/joke/Any").then(
     (response) => response.json()
   );
+  if (chiste.category == "Dark"){
+    return {
+      joke: "Oops, un chiste de humor negro, mejor elegimos otro"
+    }
+  }
   return chiste;
 }
