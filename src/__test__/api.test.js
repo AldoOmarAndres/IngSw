84eccb02c647c405d0bb64d466a162ec4b30f27d
Aldo <aldoandres045@gmail.com>
2024-06-02 17:03:25 -0300

    KAN-2 tests corregidos, prueba deploy 2

--- src/__test__/api.test.js
+++ src/__test__/api.test.js
@@ -1,8 +1,7 @@
 import { traerChiste } from '../api';
 
-describe('traerChiste', () => {
-  test('debe devolver un objeto de chiste correctamente', async () => {
-    const chiste = await traerChiste();
-    expect(chiste.error).toBe(false);
-  });
+
+test('debe devolver un objeto de chiste correctamente', async () => {
+  const chiste = await traerChiste();
+  expect(chiste.error).toBe(false);
 });
